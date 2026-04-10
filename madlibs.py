6c7b3113bb3e9a4b7634759f94e6ad6a881c546c
raynnz0907 <rayan310807@gmail.com>
2026-04-10 21:24:57 +0530

    printing the words

--- madlibs.py
+++ madlibs.py
@@ -17,3 +17,13 @@ for i, char in enumerate(story):
         words.add(word)
         start_of_word = -1
 
+answers = {}
+
+for word in words:
+    answer = input(f"Enter a word for {word}: ")
+    answers[word] = answer
+
+for word in words:
+    story = story.replace(word, answers[word])
+
+print(story)
